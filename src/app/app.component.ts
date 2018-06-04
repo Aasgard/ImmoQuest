4c6e546185494083855a801b1efb230db61f1b14
Francois-Regis LANCIEN <francois-regis.lancien@niji.fr>
2018-06-04 11:05:49 +0200

    feature(map): Added some tests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {IMarker} from './models/marker.model';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  zoom = 14;
-  lat = 48.118845;
-  lng = -1.651217;
+
+    zoom = 13;
+    lat = 48.118845;
+    lng = -1.651217;
+    public markers: IMarker[] = [];
+
+    public onMapClicked(event: any) {
+        this.markers = [];
+        this.markers.push({lat: event.coords.lat, lng: event.coords.lng, label: '♥'});
+    }
+
+    public onMapRightClicked(event: any) {
+        alert('Modal de gestion pour le clic droit');
+    }
 }
