99fa1dc296cfd352e40338196f6267c96fc90e50
François-Régis Lancien <francois.regis.lancien@gmail.com>
2018-06-06 23:50:41 +0200

    feature(map): Added navbar and loader

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,22 +8,30 @@ import {environment} from '../environments/environment';
 import {AngularFireModule} from 'angularfire2';
 import {AngularFireAuthModule} from 'angularfire2/auth';
 import {AngularFireDatabase} from 'angularfire2/database';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatProgressBarModule} from '@angular/material';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
-    declarations: [
-        AppComponent
-    ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        AngularFireModule.initializeApp(environment.firebase),
-        AngularFireAuthModule,
-        AgmCoreModule.forRoot({
-            apiKey: 'AIzaSyDOSn5kyr3PBRT03oZ8L7rxHx8LsSd-oQk'
-        })
-    ],
-    providers: [AngularFireDatabase],
-    bootstrap: [AppComponent]
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatProgressBarModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyDOSn5kyr3PBRT03oZ8L7rxHx8LsSd-oQk'
+    })
+  ],
+  providers: [AngularFireDatabase],
+  bootstrap: [AppComponent]
 })
 export class AppModule {
 }
